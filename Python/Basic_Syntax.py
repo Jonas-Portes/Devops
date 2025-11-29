05c257c296b17662f48df37be4d6dd9c16caef82
Jonas Portes <jonas.portes@hotmail.com>
2025-11-28 22:23:41 -0300

    First

---
+++ Python/Basic_Syntax.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+# <- Used to Comment
+# Syntax defines a set of rules that are used to create a Program.
+# Python Syntax have has many similarities to Perl, C, and Java Programming Languages.
+# However, there are some definite differences between the languages.
+# Python is an iterpreted language (But it can be compiled)
+
+# To enter Python interavtive Mode from Terminal (or prompt) use 'python3'
+
+# Basic Syntaxes
+
+print("Hello world!") # Bless the first print ever
+
+print() # Used to give space to text and give context
+
+print("Here we can write thing we want the program to...")
+print("print")
+
+print()
+
+print("By default, on the end of all pythons 'prints' there's this regex '\\n'") # \n
+
+print()
+
+print("But of course we can change it using 'args', basically we say 'end='XXX'' where XXX is the new regex or word, character, you choose")
+print("It also need a comma between the real text, ex:  print(\"original text\", end='I know Python')")
+
+print()
+
+print("To test it, let's use 'I know Python' on the end", end='I know Python')
+print("But... we are TOO CLOSE")
+
+print()
+print()
+
+print("Removing the normal '\\n' makes the next one to print together with the first, so we may need to use spaces, like so")
+
+print("To test it, let's use 'I know Python' on the end ", end='I know Python ')
+print("But... we are still CLOSE")
+
+print()
+
+print("This can be used better with regex or when intentionally leaving things for the user to read")
+print("That way, we can use variables and not repeat code or prints...")
+
+
+# Starting with Python Identifiers
+
+# A Python identifier is a name used to identify a variable, function, class, module or other object.
+# An identifier starts with a letter A/a to Z/z or an underscore (_) followed by zero or more letters, underscores and digits (0 to 9).
+# Python does not allow punctuation characters such as &commat;, $, and % within identifiers.
+
+# We also have NAMING CONVERNTIONS for Python identifiers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
